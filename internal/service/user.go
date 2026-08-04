74cee143035c374c1eef258adec7e65e975d883d
srshkn <snuqlstyas@gmail.com>
2026-08-05 02:07:56 +0500

    adding hash password in UserService

--- internal/service/user.go
+++ internal/service/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"sketch-api-go/internal/db"
+	"sketch-api-go/internal/password"
 	"sketch-api-go/internal/repository"
 
 	v1Generated "sketch-api-go/internal/generated/v1"
@@ -23,12 +24,19 @@ func (u *UserService) Registration(
 	ctx context.Context,
 	request v1Generated.RegisterUserRequest,
 ) (db.CreateUserRow, error) {
+	var createUser db.CreateUserRow
+
+	hash, err := password.Hash(request.Password)
+	if err != nil {
+		return createUser, err
+	}
+
 	user := db.CreateUserParams{
 		Name:         request.Username,
 		Email:        string(request.Email),
-		PasswordHash: request.Password,
+		PasswordHash: hash,
 	}
-	createUser, err := u.repository.CreateUser(ctx, user)
+	createUser, err = u.repository.CreateUser(ctx, user)
 	if err != nil {
 		return createUser, err
 	}
