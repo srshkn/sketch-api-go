a25117be7f9c8b45c0697f0c059a202ec597da6a
srshkn <snuqlstyas@gmail.com>
2026-08-11 04:50:14 +0500

    add package errs

---
+++ internal/errs/errors.go
@@ -0,0 +1,13 @@
+package errs
+
+import (
+	"errors"
+)
+
+var (
+	ErrNotFound     = errors.New("not found")
+	ErrUnauthorized = errors.New("unauthorized")
+	ErrForbidden    = errors.New("forbidden")
+	ErrInvalidInput = errors.New("invalid input")
+	ErrConflict     = errors.New("conflict")
+)
