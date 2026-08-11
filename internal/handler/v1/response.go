742a65ae7caffa7513ccd032cf529471818b7650
srshkn <snuqlstyas@gmail.com>
2026-08-12 01:17:21 +0500

    add response funcs

---
+++ internal/handler/v1/response.go
@@ -0,0 +1,33 @@
+package v1
+
+import (
+	"encoding/json"
+	"net/http"
+
+	v1Generated "sketch-api-go/internal/generated/v1"
+)
+
+func writeJSON(
+	w http.ResponseWriter,
+	status int,
+	value any,
+) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_ = json.NewEncoder(w).Encode(value)
+}
+
+func writeError(
+	w http.ResponseWriter,
+	status int,
+	code v1Generated.ErrorResponseErrorCode,
+	message string,
+) {
+	response := v1Generated.ErrorResponse{}
+
+	response.Error.Code = code
+	response.Error.Message = message
+
+	writeJSON(w, status, response)
+}
